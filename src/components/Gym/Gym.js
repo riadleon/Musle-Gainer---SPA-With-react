6d7e412e0e9188554c0f29d02b4ced0f9e021c00
riadshalahinleon <riadshalahinleon@gmail.com>
2022-09-29 12:11:41 +0600

    fakejson added

--- src/components/Gym/Gym.js
+++ src/components/Gym/Gym.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import './Gym.css'
 
 const Gym = () => {
     return (
-        <div>
-            <h2>Leo's Muscle Gainer</h2>
+        <div className='gym-container'>
+            <div className="exercises-container">
+                <h1>Muscle Gainer</h1>
+            </div>
+            <div className="details-container">
+
+            </div>
         </div>
     );
 };
