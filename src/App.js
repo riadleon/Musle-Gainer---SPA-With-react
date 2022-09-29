c9cb3baa83cd761090887afb3298d2386a6439c7
riadshalahinleon <riadshalahinleon@gmail.com>
2022-09-29 13:14:43 +0600

    tittle added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
 import Gym from './components/Gym/Gym';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <Gym></Gym>
     </div>
   );
---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDumbbell } from '@fortawesome/free-solid-svg-icons'
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <FontAwesomeIcon icon={faDumbbell}></FontAwesomeIcon>
+            <h2 className='tittle-txt'>Leo's Muscle Gainer</h2>
+        </div>
+    );
+};
+
+export default Header;
