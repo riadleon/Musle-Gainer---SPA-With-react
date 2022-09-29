a859a850e5a55ac3363ea445cfe654e4a8a3e065
riadshalahinleon <riadshalahinleon@gmail.com>
2022-09-29 14:18:42 +0600

    responsiveness done

--- src/components/Gym/Gym.js
+++ src/components/Gym/Gym.js
@@ -55,8 +55,21 @@ const Gym = () => {
 
 
                 </div>
+                <div className="time-details">
+                    <h3>Exercies Details</h3>
+                    <div className="input-field">
+                        <div className="timeRequired">
+                            <label htmlFor="">Exercise Time:</label>
+                            <input type="text" />
+                        </div>
+                        <div className="breakRequired">
+                            <label htmlFor="">Break Time:</label>
+                            <input type="text" />
+                        </div>
+                    </div>
+                </div>
 
-
+                <button className='btn-activity'>Activity Completed</button>
             </div>
         </div>
     );
