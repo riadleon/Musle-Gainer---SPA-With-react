5ff94ea8c3895453bc38c263f556d999cfd54adb
riadshalahinleon <riadshalahinleon@gmail.com>
2022-09-29 13:36:15 +0600

    perosnal details part

--- src/components/Gym/Gym.js
+++ src/components/Gym/Gym.js
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLocation } from '@fortawesome/free-solid-svg-icons'
+
 import React, { useEffect, useState } from 'react';
 import Exercise from '../Exercise/Exercise';
 import './Gym.css'
@@ -5,23 +8,45 @@ import './Gym.css'
 const Gym = () => {
     const [exercises, setExercises] = useState([]);
 
-    useEffect( () => {
-            fetch('Gym.json')
+    useEffect(() => {
+        fetch('Gym.json')
             .then(res => res.json())
             .then(data => setExercises(data))
     }, []);
     return (
         <div className='gym-container'>
             <div className="exercises-container">
-                
+
                 {
-                    exercises.map(exercise => <Exercise 
+                    exercises.map(exercise => <Exercise
                         key={exercise.id}
-                        exercise = {exercise}
-                         ></Exercise>)
+                        exercise={exercise}
+                    ></Exercise>)
                 }
             </div>
             <div className="details-container">
+                <div className="p-details">
+                    <h3>Riad Shalahin Leon</h3>
+                    <div className="location">
+                        <FontAwesomeIcon icon={faLocation}></FontAwesomeIcon>
+                        <p>Dhaka, Bangladesh</p>
+                    </div>
+                    <div className="details">
+                        <p>72 kg</p>
+                        <p>5.8"</p>
+                        <p>24 Years</p>
+                    </div>
+                </div>
+
+                <div className="break">
+                    <p>10s</p>
+                    <p>20s</p>
+                    <p>30s</p>
+                    <p>40s</p>
+                    <p>50s</p>
+
+                </div>
+
 
             </div>
         </div>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ import './Header.css'
 const Header = () => {
     return (
         <div className='header'>
-            <FontAwesomeIcon icon={faDumbbell}></FontAwesomeIcon>
+            <FontAwesomeIcon className='tittle-txt' icon={faDumbbell}></FontAwesomeIcon>
             <h2 className='tittle-txt'>Leo's Muscle Gainer</h2>
         </div>
     );
